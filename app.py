a3644646b8e9876257aca6c33c8f21f7bfea38f7
Liz Acosta <lacosta@50-0-190-44.dsl.dynamic.fusionbroadband.com>
2018-11-23 21:08:23 -0800

    Got texting to work

--- app.py
+++ app.py
@@ -4,22 +4,28 @@ import csv
 import random
 from random import shuffle
 from twilio.rest import Client
+import os
+from dotenv import load_dotenv, find_dotenv
 
 app = Flask(__name__)
 
-account_sid = 
-auth_token = 
+load_dotenv(find_dotenv())
+
+account_sid = os.getenv('ACCOUNT_SID')
+auth_token = os.getenv('ACCOUNT_TOKEN')
+account_number = os.getenv('ACCOUNT_NUMBER')
+
 client = Client(account_sid, auth_token)
 
 tarot_tsv_file = open("tarot-deck.tsv", "r")
 # Open tsv Tarot deck file
 TAROT_DECK_DICT = {}
 
+tarot_reading = []
+
 with open("tarot-deck.tsv") as tsv:
 	for line in csv.reader(tsv, dialect="excel-tab"):
 		TAROT_DECK_DICT[line[0]] = line[1:]
-
-print(TAROT_DECK_DICT)
 
 @app.route("/")
 def home():
@@ -41,7 +47,6 @@ def deck_selection():
 @app.route("/final_reading", methods=['GET', 'POST'])
 def final_five():
 	final_cards = random.sample(xrange(78),3)
-	tarot_reading = []
 	for card in final_cards:
 		tarot_reading.append(TAROT_DECK_DICT[str(card)])
 	text = "Here are your cards ..."
@@ -50,10 +55,16 @@ def final_five():
 
 @app.route("/text", methods=['GET', 'POST'])
 def text_reading():
-	number = request.form
-	print(number)
+	number = request.form.getlist('phonenumber')
+	final_reading = []
+	for card in tarot_reading:
+		final_reading.append(card[1])
+
+	message_body = '; '.join(final_reading)
+
+	client.messages.create(from_=account_number, body=message_body, to=number[0])
 	
-	return render_template("index.html")
+	return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
